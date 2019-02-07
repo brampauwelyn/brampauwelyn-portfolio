2cbcc484947c7971907dbbbbd7a8697efd389586
brampauwelyn <brampauw@gmail.com>
2019-02-07 12:12:53 +0100

    handle forms via netlify

--- src/components/CurrentTrack.vue
+++ src/components/CurrentTrack.vue
@@ -20,77 +20,81 @@ import axios from 'axios';
 
 export default {
   name: 'CurrentTrack',
-  data: function(){
-   return{
-     artist: '',
-     song: '',
-     image: '',
-     loaded: false
-   }
+  data: function () {
+    return {
+      artist: '',
+      song: '',
+      image: '',
+      loaded: false
+    }
   },
   methods: {
-    getCurrentTrack: function(){
+    getCurrentTrack: function () {
       const user = 'bram_pauwelyn';
       const api_key = '6cd40f6787867ede2c5aa4e2f103be06';
       const url = `https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=${user}&api_key=${api_key}&limit=1&format=json`;
       axios.get(url)
-      .then( (res) => {
-        const song = res.data.recenttracks.track[0];
-        this.song = song.name;
-        this.image = song.image[3]["#text"];
-        this.artist = song.artist["#text"];
-        this.loaded = true;
-      })
+        .then((res) => {
+          const song = res.data.recenttracks.track[0];
+          this.song = song.name;
+          this.image = song.image[3]["#text"];
+          this.artist = song.artist["#text"];
+          this.loaded = true;
+        })
     }
   },
-  created(){
+  created() {
     this.getCurrentTrack()
   }
 }
 </script>
 
 <style lang="scss">
-  #current-track{
+  #current-track {
     padding: 20px 40px;
     border-radius: 10px;
-    background-color: rgba(0,0,0,0.9);
+    background-color: rgba(0, 0, 0, 0.9);
     position: fixed;
     bottom: 30px;
     right: 30px;
     z-index: 1000;
     color: #fff;
     padding: 20px;
-        -webkit-animation: fadein 2s; /* Safari, Chrome and Opera > 12.1 */
-       -moz-animation: fadein 2s; /* Firefox < 16 */
-        -ms-animation: fadein 2s; /* Internet Explorer */
-         -o-animation: fadein 2s; /* Opera < 12.1 */
-            animation: fadein 2s;
+    -webkit-animation: fadein 2s;
+    /* Safari, Chrome and Opera > 12.1 */
+    -moz-animation: fadein 2s;
+    /* Firefox < 16 */
+    -ms-animation: fadein 2s;
+    /* Internet Explorer */
+    -o-animation: fadein 2s;
+    /* Opera < 12.1 */
+    animation: fadein 2s;
   }
 
-  .track-intro{
-    p{
+  .track-intro {
+    p {
       margin-bottom: 10px;
     }
   }
 
-  .track-info{
+  .track-info {
     display: flex;
     justify-content: space-between;
     align-items: center;
   }
 
-  .cover__thumbnail{
+  .cover__thumbnail {
     position: relative;
     margin-right: 10px;
     border-radius: 50%;
     overflow: hidden;
   }
 
-  .cover__image{
+  .cover__image {
     width: 50px;
   }
 
-  .cover__equalizer{
+  .cover__equalizer {
     position: absolute;
     width: 100%;
     height: 100%;
@@ -100,11 +104,11 @@ export default {
     left: 0;
   }
 
-  .artist{
+  .artist {
     font-size: .9em;
   }
 
-  .bar{
+  .bar {
     display: block;
     width: 5px;
     height: 10px;
@@ -114,17 +118,17 @@ export default {
   }
 
 
-@keyframes fadein {
-    from { 
-      opacity: 0; 
+  @keyframes fadein {
+    from {
+      opacity: 0;
       transform: scale(0) translateX(3000px);
     }
-    to   { 
-      opacity: 1;
-      transform: scale(1) translate(0,0);
-    }
-}
 
+    to {
+      opacity: 1;
+      transform: scale(1) translate(0, 0);
+    }
+  }
 
 
 </style>
